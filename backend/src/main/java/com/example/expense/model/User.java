0c080ba3a711e5c8cd21ce6db8008d8b30ca30a6
Romil Jain <romil2005@gmail.com>
2020-06-30 01:09:50 -0400

    Remove Roles from users (#3)

--- backend/src/main/java/com/example/expense/model/User.java
+++ backend/src/main/java/com/example/expense/model/User.java
@@ -1,16 +1,12 @@
 package com.example.expense.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import java.util.HashSet;
-import java.util.Set;
 
 @NoArgsConstructor
 @Data
@@ -31,9 +27,6 @@ public class User {
 
     @NotBlank
     private String password;
-
-    @DBRef
-    private Set<Role> roles = new HashSet<>();
 
     public User(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
--- backend/src/main/java/com/example/expense/payload/response/JwtResponse.java
+++ backend/src/main/java/com/example/expense/payload/response/JwtResponse.java
@@ -2,20 +2,16 @@ package com.example.expense.payload.response;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class JwtResponse {
     private String token;
     private String id;
     private String email;
-    private List<String> roles;
     private String type = "Bearer";
 
-    public JwtResponse(String token, String id, String email, List<String> roles) {
+    public JwtResponse(String token, String id, String email) {
         this.token = token;
         this.id = id;
         this.email = email;
-        this.roles = roles;
     }
 }
