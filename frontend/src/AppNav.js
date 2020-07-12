8ff8f91a5d0d7bd298ae4f5d20c932ed303e1b21
Romil Jain <romil2005@gmail.com>
2020-07-12 19:12:08 -0400

    Add logout functionality (#7)

--- frontend/src/AppNav.js
+++ frontend/src/AppNav.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import AuthService from "./AuthService";
 
 class AppNav extends Component {
-  state = {};
+  constructor(props) {
+    super();
+    this.state = {
+      loggedIn: false,
+    };
+
+    this.onClickLogout = this.onClickLogout.bind(this);
+  }
+
+  async componentDidMount() {
+    if (AuthService.isLoggedIn()) {
+      this.setState({
+        loggedIn: true,
+      });
+    }
+  }
+
+  onClickLogout() {
+    AuthService.logout();
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +34,12 @@ class AppNav extends Component {
             <Nav.Link href="/expenses">Expenses</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="/login">Login</Nav.Link>
+            {!this.state.loggedIn && (
+              <Nav.Link href="/login">Login</Nav.Link>
+            )}
+            {this.state.loggedIn && (
+              <Nav.Link href="/" onClick={this.onClickLogout}>Logout</Nav.Link>
+            )}
           </Nav>
         </Navbar>
       </div>
